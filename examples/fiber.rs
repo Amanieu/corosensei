6b9a2dd2febe0320098165e416242250b20d9ebb
Daria Sukhonina <dariasukhonina@gmail.com>
2024-10-10 17:34:43 +0300

    rename execution to fiber in the fiber example source code

--- examples/fiber.rs
+++ examples/fiber.rs
@@ -1,3 +1,5 @@
+// This example is basically a reworked `example/basic.rs`
+
 use corosensei::Fiber;
 
 struct Arg {
@@ -17,26 +19,26 @@ impl Arg {
 }
 
 fn main() {
-    println!("[main] creating execution");
+    println!("[main] creating fiber");
 
     let mut exec = Fiber::new(|Arg { mut exec, i }| {
-        println!("[execution] execution started with input {}", i);
+        println!("[fiber] fiber started with input {}", i);
         for mut i in 0..5 {
-            println!("[execution] yielding {}", i);
+            println!("[fiber] yielding {}", i);
             Arg { exec, i } = exec.switch(move |exec| Some(Yield { exec, i }));
-            println!("[execution] got {} from parent", i)
+            println!("[fiber] got {} from parent", i)
         }
-        println!("[execution] exiting execution");
+        println!("[fiber] exiting fiber");
         (exec, |_| None)
     });
 
     let mut counter = 100;
     loop {
-        println!("[main] resuming execution with argument {}", counter);
+        println!("[main] resuming fiber with argument {}", counter);
         match exec.switch(move |exec| Arg::new(exec, counter)) {
             Some(Yield { exec: new, i }) => {
                 exec = new;
-                println!("[main] got {:?} from execution", i)
+                println!("[main] got {:?} from fiber", i)
             }
             None => break,
         }
@@ -48,6 +50,6 @@ fn main() {
 }
 
 #[test]
-fn execution() {
+fn fiber() {
     main()
 }
